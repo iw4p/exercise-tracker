d837dbbffe1ae57136af5b6d1fc37a17ddc45718
Nima Akbarzade <Nimakbarzade@yahoo.com>
2020-06-08 20:52:22 +0430

    complete UI

--- mern-exercise-tracker/src/App.js
+++ mern-exercise-tracker/src/App.js
@@ -2,15 +2,23 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
 
+import Navbar from "./components/navbar.component"
+import ExercisesList from "./components/exercise-list.component"
+import EditExercise from "./components/edit-exercise.component"
+import CreateExercise from "./components/create-exercise.component"
+import CreateUser from "./components/create-user.component"
+
 function App() {
   return (
     <Router>
-      <Navbar />
-      <br/>
-      <Route path="/" exact compomemt={ExercisesList} />
-      <Route path="/edit/:id" exact compomemt={EditExercise} />
-      <Route path="/create" exact compomemt={CreateExercise} />
-      <Route path="/user" exact compomemt={CreateUser} />
+      <div className="container">
+        <Navbar />
+        <br/>
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" exact component={EditExercise} />
+        <Route path="/create" exact component={CreateExercise} />
+        <Route path="/user" exact component={CreateUser} />
+        </div>
     </Router>
   );
 }
